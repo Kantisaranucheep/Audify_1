a35f145c6f4897b10a38a65f08c578cbafa7290a
Kantisaranucheep <66011015@kmitl.ac.th>
2024-02-03 23:43:44 +0700

    include multimedia lib

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,23 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QStyle>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    : QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    int iconSize = 40;
+
+    QIcon playIcon = style()->standardIcon(QStyle::SP_MediaPlay);
+    QSize actualSize = playIcon.actualSize(QSize(iconSize, iconSize));
+    ui->push_play->setIcon(playIcon);
+    ui->push_play->setIconSize(actualSize);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
-    //asdasdasdasdsdfsdf
+    // Optionally add more cleanup code here
 }
