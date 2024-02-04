1c46f0f2c064e98029d692c1c74fd110f0729431
Kantisaranucheep <66011015@kmitl.ac.th>
2024-02-04 23:20:48 +0700

    -adding label (playlist, song) -changing border radius -changing theme to black

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,9 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include <QStyle>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QFont>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent),
@@ -14,6 +17,26 @@ MainWindow::MainWindow(QWidget *parent)
     QSize actualSize = playIcon.actualSize(QSize(iconSize, iconSize));
     ui->push_play->setIcon(playIcon);
     ui->push_play->setIconSize(actualSize);
+
+    QIcon skipIcon = style()->standardIcon(QStyle::SP_MediaSkipForward);
+    QSize skipIconSize = skipIcon.actualSize(QSize(iconSize, iconSize));
+    ui->push_skip->setIcon(skipIcon);
+    ui->push_skip->setIconSize(skipIconSize);
+
+    QIcon backIcon = style()->standardIcon(QStyle::SP_MediaSkipBackward);
+    QSize backIconSize = backIcon.actualSize(QSize(iconSize, iconSize));
+    ui->push_back->setIcon(backIcon);
+    ui->push_back->setIconSize(backIconSize);
+
+    QIcon volumeIcon = style()->standardIcon(QStyle::SP_MediaVolume);
+    QSize volumeIconSize = volumeIcon.actualSize(QSize(iconSize, iconSize));
+    ui->volume->setIcon(volumeIcon);
+    ui->volume->setIconSize(volumeIconSize);
+
+    // QLabel *label = new QLabel("Playlist");  // Create a QLabel with the text "Playlist"
+
+    // QVBoxLayout *frameLayout = new QVBoxLayout(ui->frame_2);  // Assuming frame_2 is your QFrame
+    // frameLayout->addWidget(label);
 }
 
 MainWindow::~MainWindow()
