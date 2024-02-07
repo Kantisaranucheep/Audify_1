c9478f3cd030cce072a9388abce86b0e400bd521
Kantisaranucheep <66011015@kmitl.ac.th>
2024-02-07 14:00:00 +0700

    7/2/2024 update

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,43 +4,136 @@
 #include <QLabel>
 #include <QVBoxLayout>
 #include <QFont>
+#include <QDebug>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    ui(new Ui::MainWindow),
+    isPause(false)
 {
     ui->setupUi(this);
 
+    MPlayer = new QMediaPlayer();
+    audioOutput = new QAudioOutput();
+
     int iconSize = 40;
 
-    QIcon playIcon = style()->standardIcon(QStyle::SP_MediaPlay);
-    QSize actualSize = playIcon.actualSize(QSize(iconSize, iconSize));
-    ui->push_play->setIcon(playIcon);
-    ui->push_play->setIconSize(actualSize);
+    ui->push_play->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
+    ui->push_play->setIconSize(QSize(iconSize, iconSize));
 
-    QIcon skipIcon = style()->standardIcon(QStyle::SP_MediaSkipForward);
-    QSize skipIconSize = skipIcon.actualSize(QSize(iconSize, iconSize));
-    ui->push_skip->setIcon(skipIcon);
-    ui->push_skip->setIconSize(skipIconSize);
+    ui->push_skip->setIcon(style()->standardIcon(QStyle::SP_MediaSkipForward));
+    ui->push_skip->setIconSize(QSize(iconSize, iconSize));
 
-    QIcon backIcon = style()->standardIcon(QStyle::SP_MediaSkipBackward);
-    QSize backIconSize = backIcon.actualSize(QSize(iconSize, iconSize));
-    ui->push_back->setIcon(backIcon);
-    ui->push_back->setIconSize(backIconSize);
+    ui->push_back->setIcon(style()->standardIcon(QStyle::SP_MediaSkipBackward));
+    ui->push_back->setIconSize(QSize(iconSize, iconSize));
 
-    QIcon volumeIcon = style()->standardIcon(QStyle::SP_MediaVolume);
-    QSize volumeIconSize = volumeIcon.actualSize(QSize(iconSize, iconSize));
-    ui->volume->setIcon(volumeIcon);
-    ui->volume->setIconSize(volumeIconSize);
+    ui->volume->setIcon(style()->standardIcon(QStyle::SP_MediaVolume));
+    ui->volume->setIconSize(QSize(iconSize, iconSize));
 
-    // QLabel *label = new QLabel("Playlist");  // Create a QLabel with the text "Playlist"
+    ui->horizontalSlider_2->setMinimum(1);
+    ui->horizontalSlider_2->setMaximum(100);
+    ui->horizontalSlider_2->setValue(30);
 
-    // QVBoxLayout *frameLayout = new QVBoxLayout(ui->frame_2);  // Assuming frame_2 is your QFrame
-    // frameLayout->addWidget(label);
+    connect(MPlayer, &QMediaPlayer::durationChanged, this, &MainWindow::durationChanged);
+    connect(MPlayer, &QMediaPlayer::positionChanged, this, &MainWindow::positionChanged);
+
+
 }
+
 
 MainWindow::~MainWindow()
 {
     delete ui;
     // Optionally add more cleanup code here
+}
+
+void MainWindow::durationChanged(qint64 duration) {
+    qint64 totalSeconds = duration / 1000;
+    QTime durationTime((totalSeconds / 3600) % 60, (totalSeconds / 60) % 60, totalSeconds % 60);
+    QTime progressTime();
+    ui->label_duration2->setText(durationTime.toString("mm:ss"));
+
+}
+
+void MainWindow::positionChanged(qint64 progress) {
+
+}
+
+
+void MainWindow::on_push_play_clicked()
+{
+    if (isPause == false) {
+        // Resume functionality
+        QIcon playIcon = style()->standardIcon(QStyle::SP_MediaPlay);
+        ui->push_play->setIcon(playIcon);
+
+        isPause = true;
+    } else {
+        // Pause functionality
+        QIcon pauseIcon = style()->standardIcon(QStyle::SP_MediaPause);
+        ui->push_play->setIcon(pauseIcon);
+        MPlayer->pause();
+
+        isPause = false;
+    }
+}
+
+void MainWindow::on_push_skip_clicked()
+{
+
+}
+
+void MainWindow::on_push_back_clicked()
+{
+
+}
+
+
+void MainWindow::on_actionOpen_Audio_File_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Select Audio File"), "", tr("MP3 Files (*.mp3)"));
+
+    if (!fileName.isEmpty()) {
+        // Set media content to the selected file
+        MPlayer->setSource(QUrl::fromLocalFile(fileName));
+
+        // Update the label with the file name
+        QFileInfo fileInfo(fileName);
+        ui->label_File_Name->setText(fileInfo.fileName());
+
+        // Optional: Start playing the media when a new file is loaded
+        MPlayer->play();
+    }
+}
+
+void MainWindow::on_volume_clicked()
+{
+    if (IS_MUTE == false) {
+        QIcon volumeIcon = style()->standardIcon(QStyle::SP_MediaVolume);
+        ui->volume->setIcon(volumeIcon);
+        audioOutput->setMuted(false); // Set to false to unmute
+        IS_MUTE = true;
+    } else {
+        QIcon volumeMutedIcon = style()->standardIcon(QStyle::SP_MediaVolumeMuted);
+        ui->volume->setIcon(volumeMutedIcon);
+        audioOutput->setMuted(true); // Set to true to mute
+        IS_MUTE = false;
+    }
+}
+
+void MainWindow::on_horizontalSlider_2_valueChanged(int value)
+{
+    audioOutput->setVolume(value / 100.0); // Set volume between 0.0 and 1.0
+}
+
+void MainWindow::on_horizontalSlider_sliderMoved(int position)
+{
+    audioOutput->setVolume(position / 100.0);
+
+    if (position == 0) {
+        ui->volume->setIcon(style()->standardIcon(QStyle::SP_MediaVolumeMuted));
+    } else {
+        ui->volume->setIcon(style()->standardIcon(QStyle::SP_MediaVolume));
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,9 @@
 
 #include <QMainWindow>
 #include <QtMultiMedia>
+#include <QMediaPlayer>
+#include <QtWidgets>
+#include <QAudioOutput>
 
 
 QT_BEGIN_NAMESPACE
@@ -19,7 +22,35 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+
+    void durationChanged(qint64 duration);
+    void positionChanged(qint64 progress);
+
+    void on_push_play_clicked();
+
+    void on_push_skip_clicked();
+
+
+    void on_push_back_clicked();
+
+    void on_actionOpen_Audio_File_triggered();
+
+    void on_volume_clicked();
+
+    void on_horizontalSlider_2_valueChanged(int value);
+
+    void on_horizontalSlider_sliderMoved(int position);
+
 private:
     Ui::MainWindow *ui;
+    bool IS_MUTE = false;
+    bool isPause = false;
+    QMediaPlayer *MPlayer;
+    QAudioOutput *audioOutput;
+
+    qint64 duration;
+    void updateDuration(qint64 duration);
+
 };
 #endif // MAINWINDOW_H
